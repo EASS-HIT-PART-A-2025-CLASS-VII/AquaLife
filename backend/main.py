02a8abf0b24db796745d4959c532377c9adb0ba1
shaked6133 <sabag975@gmail.com>
2025-05-12 17:28:19 +0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,8 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1",
         "http://127.0.0.1:3000"
-    ],  # Allow both localhost and localhost:3000
+    ],  
+    # Allow both localhost and localhost:3000
     allow_credentials=True,
     allow_methods=["*"],  # Allows all methods
     allow_headers=["*"],  # Allows all headers
